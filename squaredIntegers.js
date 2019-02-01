02409303e77f53065694843adf3169f2884f6aa2
Fernando G <kev9504@gmail.com>
2019-02-01 02:40:31 +0000

    completed

---
+++ squaredIntegers.js
@@ -0,0 +1,6 @@
+"use strict";
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = arr => arr.filter(el=> Number.isInteger(el) && el > 0).map(val=>val*val);
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
+
